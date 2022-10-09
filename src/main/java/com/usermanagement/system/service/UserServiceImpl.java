092e5f362160d980e325ae88fa9b78a53148fda1
Ludmilla <ludmillajani@yahoo.com>
2022-10-09 11:53:44 -0400

    saveuser Impl

---
+++ src/main/java/com/usermanagement/system/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.usermanagement.system.service;
+
+import com.usermanagement.system.entity.UserEntity;
+import com.usermanagement.system.model.User;
+import com.usermanagement.system.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+        userRepository.save(userEntity);
+        return user;
+    }
+
+
+}
+
+
+
